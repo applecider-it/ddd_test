8b08ae41ee7eb88df400f468fa1bb4355e9502bb
owner <owner@localhost>
2026-03-15 10:42:58 +0900

    細かい変更

--- src/app/controllers/posts_controller.rb
+++ src/app/controllers/posts_controller.rb
@@ -1,10 +1,9 @@
 class PostsController < ApplicationController
-  before_action :setup
   before_action :set_post, only: %i[ show edit update destroy ]
 
   # GET /posts or /posts.json
   def index
-    @posts = Posts::UseCases::AllPost.new(@post_repository).call
+    @posts = Posts::UseCases::AllPost.new(post_repository).call
   end
 
   # GET /posts/1 or /posts/1.json
@@ -30,7 +29,7 @@ class PostsController < ApplicationController
         title: @post_form.title,
         content: @post_form.content,
       )
-      new_id = Posts::UseCases::CreatePost.new(@post_repository).call(post_dto)
+      new_id = Posts::UseCases::CreatePost.new(post_repository).call(post_dto)
       redirect_to post_path(new_id), notice: "Post was successfully created."
     else
       render :new, status: :unprocessable_entity
@@ -45,7 +44,7 @@ class PostsController < ApplicationController
     if @post_form.valid?
       @post_dto.title = @post_form.title
       @post_dto.content = @post_form.content
-      Posts::UseCases::UpdatePost.new(@post_repository).call(@post_dto)
+      Posts::UseCases::UpdatePost.new(post_repository).call(@post_dto)
 
       redirect_to post_path(@post_dto.id), notice: "Post was successfully updated.", status: :see_other
     else
@@ -55,7 +54,7 @@ class PostsController < ApplicationController
 
   # DELETE /posts/1 or /posts/1.json
   def destroy
-    Posts::UseCases::DeletePost.new(@post_repository).call(
+    Posts::UseCases::DeletePost.new(post_repository).call(
       id: @post_dto.id,
     )
 
@@ -63,13 +62,13 @@ class PostsController < ApplicationController
   end
 
   private
-    def setup
-      @post_repository = Posts::Repositories::PostRepository.new
+    def post_repository
+      @post_repository ||= Posts::Repositories::PostRepository.new
     end
 
     # Use callbacks to share common setup or constraints between actions.
     def set_post
-      @post_dto = Posts::UseCases::FindPost.new(@post_repository).call(
+      @post_dto = Posts::UseCases::FindPost.new(post_repository).call(
         id: params.expect(:id),
       )
       @post_form = Posts::PostForm.new

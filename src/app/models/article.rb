44d641fe1e73abd8f7afa9dd495cfe531d051667
owner <owner@localhost>
2026-04-01 14:45:37 +0900

    markdown追加

--- src/app/models/article.rb
+++ src/app/models/article.rb
@@ -1,4 +1,15 @@
+require 'redcarpet'
+
 class Article < ApplicationRecord
   validates :title, presence: true, length: { maximum: 100 }
   validates :content, presence: true
+
+  # 内容をmarkdown変換したHTML
+  def content_html
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+
+    html = markdown.render(content)
+
+    html
+  end
 end

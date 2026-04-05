5fe082ec8d706dcfc8c442cfd4668244eaa83b28
owner <owner@localhost>
2026-04-05 12:54:47 +0900

    細かい変更

--- src/app/controllers/posts_controller.rb
+++ src/app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
 
   # GET /posts or /posts.json
   def index
-    @posts = Posts::UseCases::AllPost.new(post_repository).call
+    @posts_dto = Posts::UseCases::AllPost.new(post_repository).call
   end
 
   # GET /posts/1 or /posts/1.json

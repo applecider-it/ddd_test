7c59161c6e285aa73f31c8e1ddea07296ec354e4
owner <owner@localhost>
2026-04-05 12:17:18 +0900

    日時動作確認

--- src/config/application.rb
+++ src/config/application.rb
@@ -23,5 +23,15 @@ module Myapp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # デフォルト言語を日本語に設定
+    config.i18n.default_locale = :ja
+
+    # アプリケーション全体のタイムゾーン
+    config.time_zone = 'Tokyo'
+
+    # DBに保存する時のタイムゾーン
+    #config.active_record.default_timezone = :utc
+    config.active_record.default_timezone = :local
   end
 end
